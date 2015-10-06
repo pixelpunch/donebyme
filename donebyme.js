035615f47b9f1a700fbc07697529e8508800e4f7
Pixel Punch <alokdass@gmail.com>
2015-10-06 15:41:23 +0200

    Added Account Login and list creation

--- donebyme.js
+++ donebyme.js
@@ -45,7 +45,8 @@ if (Meteor.isClient) {
             Todos.insert({
                 subject: subject,
                 created_at: new Date,
-                is_done: false
+                is_done: false,
+                user_id: Meteor.userId()
             });
             var form = tmpl.find('form');
             
@@ -69,6 +70,23 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
     Meteor.publish('todos', function(){
-        return Todos.find({}, {limit:2});
+        return Todos.find({user_id: this.userId});
     })
+    
+    Todos.allow({
+    insert: function(userId, doc){
+        return userId;
+    },
+    
+    update: function(userId, doc, fieldNames, modifier){
+        return doc.user_id === userId;
+    },
+    
+    remove: function(userId, doc){
+        return doc.user_id === userId;
+    }
+    
+    
+    });
+    
 }
